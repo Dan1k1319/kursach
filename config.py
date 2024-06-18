6d171abbec8fdd1cabd1745d7d8e620a02a775c2
Dan1k1319 <danikbratko@gmail.com>
2024-06-18 09:41:13 +0300

    так, теперь чтобы все работало

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 # config.py
 import os
+import secrets
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 instance_path = os.path.join(basedir, 'instance')
 
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(instance_path, 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
